0045b17d3b7954d4bf2f685b3be86864fc5ad088
Your Name <you@example.com>
2023-09-06 17:58:18 +0200

    env

--- test_main.py
+++ test_main.py
@@ -37,5 +37,5 @@ def test_auth(client):
 
     assert response.status_code == 200
     token = response.json['token']
-    # assert token is not None
-    assert False
+    assert token is not None
+    # assert False
